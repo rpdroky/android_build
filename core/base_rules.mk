c324b3e2f85e14b35db4a1984114c6eee60b60a7
rpdroky <rpdroky@hotmail.com>
2020-05-26 10:43:50 +0300

    Revert "Add Android GO configs"
    This reverts commit 3ae8b8068453b095a3f4f22cf46a5f94c471ec83.

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -968,14 +968,6 @@ ART_BUILD_TARGET_DEBUG := false
 ART_BUILD_HOST_NDEBUG := false
 ART_BUILD_HOST_DEBUG := false
 
-# Android GO Configs
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
-
 ###########################################################
 ## NOTICE files
 ###########################################################
